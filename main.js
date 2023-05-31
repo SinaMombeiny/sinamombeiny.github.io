cc29bf5983b15dcf582b6c5dd8ab67d7dc2ef116
Sina Mombeiny <sinamombeiny@tuta.io>
2023-06-01 03:27:55 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-// script.js
 
-// Example JavaScript code to change the header text color on click
-const header = document.querySelector('header');
-header.addEventListener('click', function() {
-  header.style.color = 'red';
-});
-
-// Example JavaScript code to display an alert when the contact form is submitted
-const form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevents the form from submitting
-
-  alert('Thank you for submitting the form!');
-});
