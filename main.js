d82775d0ddee3294e3eb3b1dd0510f01c2c49f46
Sina Mombeiny <sinamombeiny@tuta.io>
2023-05-31 17:35:55 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>My Website</title>
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-  <header>
-    <h1>Welcome to My Website</h1>
-  </header>
-  
-  <main>
-    <section>
-      <h2>About</h2>
-      <p>This is a sample website created with HTML, CSS, and JavaScript.</p>
-    </section>
-    
-    <section>
-      <h2>Services</h2>
-      <ul>
-        <li>Web Design</li>
-        <li>Graphic Design</li>
-        <li>Content Writing</li>
-      </ul>
-    </section>
-    
-    <section>
-      <h2>Contact</h2>
-      <p>Email: info@example.com</p>
-      <p>Phone: 123-456-7890</p>
-    </section>
-  </main>
-  
-  <footer>
-    <p>&copy; 2023 My Website. All rights reserved.</p>
-  </footer>
+// script.js
 
-  <script src="script.js"></script>
-</body>
-</html>
+// Example JavaScript code to change the header text color on click
+const header = document.querySelector('header');
+header.addEventListener('click', function() {
+  header.style.color = 'red';
+});
+
+// Example JavaScript code to display an alert when the contact form is submitted
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevents the form from submitting
+
+  alert('Thank you for submitting the form!');
+});
