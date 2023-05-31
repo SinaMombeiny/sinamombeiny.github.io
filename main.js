015f2d1ea56a14c577682d6a96ac5e7166c991f9
Sina Mombeiny <sinamombeiny@tuta.io>
2023-05-31 17:34:39 +0330

    Update main.js

--- main.js
+++ main.js
@@ -10,15 +10,6 @@
     <h1>Welcome to My Website</h1>
   </header>
   
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-  
   <main>
     <section>
       <h2>About</h2>
