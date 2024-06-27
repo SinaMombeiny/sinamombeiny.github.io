5743f166b8520305708108db7485f1c029dd0b98
Sina Mombeiny <sina.mombeiny+github+sv34ds@icloud.com>
2024-06-27 21:43:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.remove('is-loading');
-    
-    document.querySelectorAll('#icons01 a').forEach(icon => {
-        icon.addEventListener('mouseover', () => {
-            icon.querySelector('svg').style.fill = 'rgba(217, 217, 217, 0.49)';
+
+    const icons = document.querySelectorAll('#icons01 a');
+    icons.forEach(function(icon) {
+        icon.addEventListener('mouseover', function() {
+            let svg = this.querySelector('svg');
+            if (svg) {
+                svg.style.fill = 'rgba(217, 217, 217, 0.49)';
+            }
         });
-        icon.addEventListener('mouseout', () => {
-            icon.querySelector('svg').style.fill = '#E3E3E3';
+        icon.addEventListener('mouseout', function() {
+            let svg = this.querySelector('svg');
+            if (svg) {
+                svg.style.fill = '#E3E3E3';
+            }
         });
     });
 });
