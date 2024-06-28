8307c000869a3e140d9a3c47057ac3a204340bd6
Sina Mombeiny <sina.mombeiny+github+sv34ds@icloud.com>
2024-06-28 20:32:07 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-(() => {
-    const startAnimation = () => {
-      console.log('Animation started');
+(function() {
+
+    const init = async () => {
+
+    const body = document.body;
+    body.classList.remove('is-loading');
+     startAnimation();
     };
-  
-    const init = () => {
-      if (document.body.classList.contains('is-loading')) {
-        document.body.classList.remove('is-loading');
-        setTimeout(startAnimation, 50);
-      } else {
-        startAnimation();
-      }
-    };
-  
+
     if (!document.body.classList.contains('is-loading')) {
-      document.body.classList.add('is-loading');
+    document.body.classList.add('is-loading');
     }
-  
-    if (document.readyState === 'loading') {
-      document.addEventListener('DOMContentLoaded', init);
-    } else {
-      window.requestAnimationFrame(init);
-    }
-  })();
+
+    window.addEventListener('load', init);
+
+    })();
